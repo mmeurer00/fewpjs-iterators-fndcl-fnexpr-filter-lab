d4d95e7a04f14364a244e7ca65a7e8772f640e4a
Maxine <maxinemeurer@yahoo.com>
2021-05-23 21:36:37 -0400

    commit tree 65e265cec187767b889f5c220bbb03bd87fe5d92

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(array, name) {
+    return array.filter( driver => 
+        driver.toLowerCase() === name.toLowerCase()
+    )
+}
+
+function fuzzyMatch(array, name) {
+    return array.filter( driver =>
+        driver.toLowerCase().indexOf(name.toLowerCase()) === 0
+        )
+}
+
+function matchName(array, name) {
+    return array.filter( n =>
+        n.name === name )
+}
